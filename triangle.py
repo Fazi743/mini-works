fa52191407c141e82523139ab8a2f5d259294bb0
Syed Faizan Hussain Shah <93155927+Fazi743@users.noreply.github.com>
2023-10-31 09:39:33 +0500

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,11 @@
+
+import turtle
+for i in range(3):
+    if(i==0):
+        turtle.color('green')
+    elif(i==1):
+        turtle.color('yellow')
+    elif(i==2):
+        turtle.color('red')
+    turtle.forward(50)
+    turtle.right(120)
